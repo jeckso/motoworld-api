e3110a95a617e38970509f59d2bb441d41e99ebe
DmitryFalcon <dimitreuzsokolov@gmail.com>
2019-01-04 02:26:27 -0800

    Added required dependenies to provide authorization via services.

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
+/**
+ * Main application js-file
+ * @type {createApplication}
+ */
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 3000;
+
+// path
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+var createError = require('http-errors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// region add routers
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// endregion add routers
+
+// region middleware
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+// endregion middleware
+
+// region application setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+// endregion application setup
 
 module.exports = app;
