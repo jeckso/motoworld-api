002c99bd13b11c678c4ba8348cf54c39197c5dac
DmitryFalcon <dimitreuzsokolov@gmail.com>
2019-01-19 08:27:27 -0800

    Added route for category CRUD-functions. Made minor fixes.

--- routes/order.js
+++ routes/order.js
@@ -2,23 +2,25 @@ const express = require('express');
 const router = express.Router();
 const orders = require('../controllers/order')
 
-route.post('/:id', (req, res) => {
+router.post('/:id', (req, res) => {
     orders.create(req, res);
 });
 
-route.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     orders.findById(req, res);
 });
 
-route.getAll('/', (req, res) => {
+router.getAll('/', (req, res) => {
     orders.getAll(req, res);
 });
 
-route.put('/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     orders.update(req, res);
 });
 
-route.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     orders.delete(req, res);
 });
+
+module.exports = router;
 
